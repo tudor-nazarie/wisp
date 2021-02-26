803cf7dcf2e17501e4a2d25945068eab33e20fe3
Tudor Nazarie <tudor@nazarie.net>
2021-02-26 23:42:41 +0200

    Upgrade Kotlin to version 1.4.31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.4.30"
-    kotlin("kapt") version "1.4.30"
-    kotlin("plugin.serialization") version "1.4.30"
+    kotlin("jvm") version "1.4.31"
+    kotlin("kapt") version "1.4.31"
+    kotlin("plugin.serialization") version "1.4.31"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
