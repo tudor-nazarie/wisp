afb242c63eea33e5ed03f1b92e7b804201e3b08c
Tudor Nazarie <tudor@nazarie.net>
2021-06-21 11:19:06 +0100

    Update Gradle and dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,25 +1,24 @@
 plugins {
-    kotlin("jvm") version "1.5.0"
-    kotlin("kapt") version "1.5.0"
-    kotlin("plugin.serialization") version "1.5.0"
+    kotlin("jvm") version "1.5.10"
+    kotlin("kapt") version "1.5.10"
+    kotlin("plugin.serialization") version "1.5.10"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 repositories {
     mavenCentral()
-    jcenter()
     maven(url = "https://m2.dv8tion.net/releases")
 }
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-RC")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-hocon:1.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-hocon:1.2.1")
 
-    implementation("net.dv8tion:JDA:4.2.1_262")
+    implementation("net.dv8tion:JDA:4.3.0_281")
 
     implementation("io.github.microutils:kotlin-logging:2.0.4")
     runtimeOnly("org.apache.logging.log4j:log4j-to-slf4j:2.14.0")
@@ -29,10 +28,10 @@ dependencies {
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
 
-    implementation("org.jetbrains.exposed:exposed-core:0.31.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.31.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.31.1")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.31.1")
+    implementation("org.jetbrains.exposed:exposed-core:0.32.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.32.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.32.1")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.32.1")
     implementation("org.xerial:sqlite-jdbc:3.34.0")
 }
 
